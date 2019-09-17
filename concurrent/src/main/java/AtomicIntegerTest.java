b6763d4b40d72f2941f33e9df0ebed22d29ac9a0
ouweiqi <ouweiqi@jd.com>
2019-09-17 09:30:09 +0800

    测试AtomicInteger

---
+++ concurrent/src/main/java/AtomicIntegerTest.java
@@ -0,0 +1,36 @@
+import java.sql.Time;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.*;
+import java.util.*;
+
+public class AtomicIntegerTest implements Runnable {
+    private AtomicInteger i = new AtomicInteger(0);
+    public int getValue() {
+        return i.get();
+    }
+    private void evenIncrement() {i.addAndGet(2);}
+    public void run() {
+        while (true) {
+            evenIncrement();
+        }
+    }
+    public static void main(String[] args) {
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                System.err.println("Aborting");
+                System.exit(0);
+            }
+        }, 5000);
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        AtomicIntegerTest ait = new AtomicIntegerTest();
+        executorService.execute(ait);
+        while(true) {
+            int val = ait.getValue();
+            if(val%2!=0){
+                System.out.println(val);
+                System.exit(0);
+            }
+        }
+    }
+}
